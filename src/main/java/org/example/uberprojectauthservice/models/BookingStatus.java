21aa58b1e591a26d1857f3cccf0ae3125718458a
Soumyadeep081 <soumyadeep323@gmail.com>
2026-06-08 23:09:00 +0530

    copied migrations and models and created a new migration for adding phone number

---
+++ src/main/java/org/example/uberprojectauthservice/models/BookingStatus.java
@@ -0,0 +1,10 @@
+package org.example.uberprojectauthservice.models;
+
+public enum BookingStatus {
+    SCHEDULED,
+    CANCELLED,
+    COMPLETED,
+    IN_RIDE,
+    CAR_ARRIVED,
+    ASSIGNING_DRIVER
+}
---
+++ src/main/resources/db/migration/V7__Add_email_password_phone_number_to_passenger.sql
@@ -0,0 +1,5 @@
+ALTER TABLE Passenger
+    ADD phone_number VARCHAR(255) NOT NULL UNIQUE ,
+    ADD email VARCHAR(255) NOT NULL UNIQUE ,
+    ADD password VARCHAR(255) NOT NULL ,
+    MODIFY name VARCHAR(255) NOT NULL ;
