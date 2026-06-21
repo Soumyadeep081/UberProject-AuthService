67448628e7437881422ba52857ed5858c9f38cc2
Soumyadeep081 <soumyadeep323@gmail.com>
2026-06-21 19:46:51 +0530

    Refactored models from entity service

--- src/main/java/org/example/uberprojectauthservice/UberProjectAuthServiceApplication.java
+++ src/main/java/org/example/uberprojectauthservice/UberProjectAuthServiceApplication.java
@@ -2,9 +2,17 @@ package org.example.uberprojectauthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+        "org.example.uberprojectentityservice.models"
+})
+@EnableJpaRepositories(basePackages = {
+        "org.example.uberprojectauthservice.repositories"
+})
 @EnableJpaAuditing
 public class UberProjectAuthServiceApplication {
 
--- src/main/resources/db/migration/V7__Add_email_password_phone_number_to_passenger.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE Passenger
-    ADD phone_number VARCHAR(255) NOT NULL UNIQUE ,
-    ADD email VARCHAR(255) NOT NULL UNIQUE ,
-    ADD password VARCHAR(255) NOT NULL ,
-    MODIFY name VARCHAR(255) NOT NULL ;
